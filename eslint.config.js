7637d8aab82b291532295f563e78a19552832c92
edutdutra <edutdutra@gmail.com>
2026-01-25 17:38:18 -0300

    feat: clean project initial files

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,24 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
+const importPlugin = require("eslint-plugin-import");
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: ["dist/*"],
+    plugins: {
+      import: importPlugin,
+    },
+    settings: {
+      "import/resolver": {
+        typescript: {
+          project: "./tsconfig.json",
+        },
+      },
+    },
+    rules: {
+      "import/no-unresolved": "error",
+    },
   },
 ]);
---
+++ src/app/home.tsx
@@ -0,0 +1,11 @@
+import { styles } from "@/screens/home";
+
+import { Text, View } from "react-native";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello, World!</Text>
+    </View>
+  );
+}
