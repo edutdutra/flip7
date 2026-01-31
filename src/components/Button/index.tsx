281ac315e00f09295c91ecf8f064bd565deb8026
edutdutra <edutdutra@gmail.com>
2026-01-31 10:54:43 -0300

    feat: add onPress prop to Button component for handling press events

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,11 +3,12 @@ import { styles } from "./styles";
 
 interface IButtonProps {
   text: string;
+  onPress?: () => void;
 }
 
-export function Button({ text }: IButtonProps) {
+export function Button({ text, onPress }: IButtonProps) {
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity style={styles.container} onPress={onPress}>
       <Text style={styles.text}>{text}</Text>
     </TouchableOpacity>
   );
