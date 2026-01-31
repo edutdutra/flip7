d37a5796ce264e68150592935a6edfaa1583c217
edutdutra <edutdutra@gmail.com>
2026-01-31 13:02:42 -0300

    feat: add new COLORS

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,8 +1,11 @@
 export const COLORS = {
   background: "#87cac7",
+  card: "#d6d5c5",
   text: "#fff",
 
   primary: "#30348a",
   secondary: "#fef6dc",
   tertiary: "#f9dd69",
+
+  blue: "#007cff",
 };
