deed693551d6b51acb742984f19260a818287588
edutdutra <edutdutra@gmail.com>
2026-01-31 20:14:49 -0300

    feat: update color palette in theme for improved design consistency

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -7,6 +7,26 @@ export const COLORS = {
   secondary: "#fef6dc",
   tertiary: "#f9dd69",
 
+  black: "#000000",
+  red: "#e45254",
   blue: "#007cff",
-  blue_dark: "#000000",
+  blue_dark: "#0b3b6e",
+  blue_light: "#9bd4d9",
+
+  backdrop: "rgba(0,0,0,0.4)",
+
+  card_0: "#000001",
+  card_1: "#b1a388",
+  card_2: "#d0d36c",
+  card_3: "#c64954",
+  card_4: "#5faa9f",
+  card_5: "#529c4a",
+  card_6: "#93467c",
+  card_7: "#ba736a",
+  card_8: "#acc67a",
+  card_9: "#c4844e",
+  card_10: "#c23230",
+  card_11: "#738eb8",
+  card_12: "#807464",
+  card_special: "#bf5647",
 };
